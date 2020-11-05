ed67dfd6c060a8d26ba04a0dd0d2372ebec476b6
Hiju911 <1851161310@e.tlu.edu.vn>
2020-11-05 17:59:14 +0700

    Update BTL

---
+++ header.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="contanier">
+           <div class="top">
+            <div class="top__left">
+                <a href="http://fem.tlu.edu.vn/" class="">
+                    <img src="img/logo (1).png" alt="">
+                    <img src="img/slg.png" alt="http://fem.tlu.edu.vn/">
+                </a>
+            </div>
+            <div class="top-right">
+                <ul class="header-more-info">
+                    <li><a href="#">(+) </a></li>
+                    <li>
+                        <div class="loginGroup">
+                            <a title="Login" class="LoginLink" rel="nofollow"
+                                href="login.php"> Login
+                        
+                        </div>
+                        <div class="registerGroup">
+                            <ul class="buttonGroup">
+                                <li class="userDisplayName">
+                                    <a title="Register"
+                                        href="sadregister.php">Register</a>
+
+                                </li>
+
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li>
+                        Ngôn ngữ: &nbsp;&nbsp;
+                        <a href="#" title="Tiếng Việt" class="ui-link-white lang-vi"><img src="img/icon-lang-vi.png"
+                                alt="Tiếng Việt"></a>
+                        <a href="#" title="Tiếng Anh" class=""><img src="img/icon-lang-en.png" alt="Tiếng Anh"></a>
+                    </li>
+                </ul>
+                <div class="container-search">
+                    <div class="DnnModule DnnModule-CMSSearch DnnModule-681"><a name="681"></a>
+                        <div class="Search">
+
+                            <i class="ico-search"></i>
+                            <input name="" type="text" id="" class="input-text" placeholder="Tìm kiếm"
+                                autocomplete="off">
+                            <input type="submit" name="dnn$ctr681$ViewCMSSearch$ctl00$btnSearch" value="Tìm"
+                                class="searchbutton">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg ">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"> Menu </span>
+            </button>
+            <div class="navbar-collapse  " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto ">
+                    <li class="nav-item active">
+                        <a class="nav-link text-white " href="http://www.tlu.edu.vn/">TLU <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/">TRANG CHỦ</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/gioi-thieu" id="navbarDropdown">GIỚI
+                            THIỆU </a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/thu-ngo">Thư ngỏ</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/co-cau-to-chuc">Cơ cấu tổ chức& Nhân
+                                lực </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/lich-su-hinh-thanh-phat-trien">Lịch sử
+                                hình thành
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/tin-tuc-thong-bao"
+                            id="navbarDropdown">TIN
+                            TỨC-THÔNG BÁO
+                        </a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/tin-tuc-khoa">Tin tức</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/tin-thong-bao">Thông báo </a>
+                            <div class="dropdown-divider"></div>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/dao-tao" id="navbarDropdown">ĐÀO TẠO
+                        </a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/%c4%90%c3%a0o-t%e1%ba%a1o/M%c3%b4-h%c3%acnh-%c4%91%c3%a0o-t%e1%ba%a1o">Mô
+                                hình đào tạo </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/dao-tao-dai-hoc-chinh-quy">Đại học
+                                chính quy </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/dao-tao-thac-si">Thạc sĩ </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/dao-tao-he-vua-lam-vua-hoc">Các lớp
+                                chuyên đề </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/bo-mon-trung-tam"
+                            id="navbarDropdown">BỘ
+                            MÔN-TRUNG TÂM </a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/bo-mon-ke-toan">BM Kế toán</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/bo-mon-kinh-te">BM Kinh tế </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/bo-mon-ptkn">BM Phát triển kỹ năng</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/bo-mon-quan-ly-xay-dung">BM Quản lý xây
+                                dựng</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/bo-mon-quan-tri-kinh-doanh">BM Quản trị
+                                kinh
+                                doanh</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/trung-tam-kt-ql">Trung tâm KT&Ql </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/L%c3%bd-l%e1%bb%8bch-khoa-h%e1%bb%8dc">Lý
+                                lịch khoa
+                                học</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/khoa-hoc-cong-nghe"
+                            id="navbarDropdown">KHCN-HỢP
+                            TÁC</a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/gioi-thieu-chung"> Khoan học công nghệ
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/seminar"> Seminar,Hội thảo,Hội nghị
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/cac-thao-thuan-hop-tac">Hợp tác-Tuyển
+                                dụng </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/sinh-vien" id="navbarDropdown">NGƯỜI
+                            HỌC</a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/Ng%c6%b0%e1%bb%9di-h%e1%bb%8dc/C%e1%ba%a9m-nang-ng%c6%b0%e1%bb%9di-h%e1%bb%8dc">Cẩm
+                                nang người học </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/Ng%c6%b0%e1%bb%9di-h%e1%bb%8dc/C%e1%ba%a9m-nang-ng%c6%b0%e1%bb%9di-h%e1%bb%8dc">Quy
+                                định-Biểu mẫu </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/quy-che-quy-dinh-sinh-vien">Hoạt động
+                                sinh viên </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/sinh-vien-tuong-lai">Sinh viên tương
+                                lai </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/cuu-sinh-vien">Cựu sinh viên </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="http://fem.tlu.edu.vn/ho-tro">Hỗ Trợ </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/media" id="navbarDropdown">HỌC
+                            LIỆU</a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/B%c3%a0i-gi%e1%ba%a3ng-%c4%91i%e1%bb%87n-t%e1%bb%ad">Bài
+                                giảng điện tử</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/KLTN-%c4%90ATN">KLTN-ĐATN</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/NCKH-Sinh-vi%c3%aan">NCKH
+                                Sinh viên</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/Lu%e1%ba%adn-v%c4%83n-Th%e1%ba%a1c-s%e1%bb%b9">Luận
+                                văn Thạc sỹ </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/B%c3%a0i-b%c3%a1o-Khoa-h%e1%bb%8dc">Bài
+                                báo khoa
+                                học</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"
+                                href="http://fem.tlu.edu.vn/H%e1%bb%8dc-li%e1%bb%87u/Ph%e1%ba%a7n-m%e1%bb%81m-chuy%c3%aan-d%e1%bb%a5ng">Phần
+                                mềm và tài liệu hội thảo </a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white " href="http://fem.tlu.edu.vn/">LIÊN HỆ</a>
+                    </li>
+                </ul>
+            </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
